7c6735fb73970df6947149f816fa8c1d8d7e3dc8
chriszhang94 <chris@quantumsds.com>
2020-08-27 08:36:52 +0800

    dashboard constants

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -4,3 +4,5 @@ export const AUTH_TOKEN = "AUTH_TOKEN"
 export const COLORS = ['#2a57f5', '#00C292', '#FEC108', '#e2026a',
     '#00d903', '#67d5b2', '#6890d4', '#a4e59a',
     '#f9b978', '#e23751']
+
+export const dashboard_constant = "dashboard";
