fb300050a24a53ad08a177c22be20757e60fe07c
chriszhang94 <chris@quantumsds.com>
2020-10-07 14:42:55 -0400

    invoice form and table init

--- src/View/contracts/Invoice.vue
+++ src/View/contracts/Invoice.vue
@@ -1,10 +1,69 @@
 <template>
-    <div></div>
+    <div>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item>
+                    <i class="el-icon-document"></i> Invoices
+                </el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <el-main>
+            <el-card>
+                <el-form>
+                    <el-row>
+                        <el-form-item label="Status">
+                            <el-select v-model="form.status" style="width: 100%">
+                                <el-option
+                                        v-for="item in status"
+                                        :key="item"
+                                        :label="item"
+                                        :value="item">
+                                </el-option>
+                            </el-select>
+                        </el-form-item>
+                    </el-row>
+                    <el-row>
+                        <el-form-item label="Purchase Order">
+                            <el-select v-model="form.po" style="width: 100%"></el-select>
+                        </el-form-item>
+                    </el-row>
+                    <el-row>
+                        <el-col :span="24" align="right">
+                            <el-button>Submit</el-button>
+                        </el-col>
+                    </el-row>
+                </el-form>
+            </el-card>
+            <el-card class="margin-top20">
+                <el-row>
+                    <el-tag style="width: 100%">Invoices</el-tag>
+                </el-row>
+                <el-row>
+
+                </el-row>
+            </el-card>
+        </el-main>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Invoice"
+        name: "Invoice",
+        data(){
+            return{
+                status: [
+                    'ALL',
+                    'Pending',
+                    'Approved',
+                    'Rejected'
+                ],
+                form: {
+                    status: '',
+                    po: ''
+                },
+                invoice_list: []
+            }
+        }
     }
 </script>
 
