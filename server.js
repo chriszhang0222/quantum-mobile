2a05582339d5b2aec509db97bf1f0a7b7516aae8
chriszhang94 <chris@quantumsds.com>
2020-10-15 14:22:23 -0400

    changes

--- server.js
+++ server.js
@@ -4,4 +4,18 @@ var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
+const cors = require('cors');
+app.use(cors({
+    origin:['http://localhost:5000'],
+    methods:['GET','POST'],
+}));
+
+
+app.all('*',function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+});
+
 app.listen(port);
